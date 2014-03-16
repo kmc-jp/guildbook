9d5d7464ac924a68309acecb06edf5b7dc7fd94a
Tomoki UDA <tuda@kmc.gr.jp>
2014-03-16 18:28:15 +0900

    Set default css class as .form-control
    input.form-control looks cool and {width: 100%}

--- src/view_helpers.rb
+++ src/view_helpers.rb
@@ -1,6 +1,7 @@
 module Haml
   module Helpers
     def input_userattr(user, attr, opt = {})
+      opt[:class] ||= 'form-control'
       haml_tag :input, {name: attr, value: user[attr].first}.merge(opt)
     end
   end
