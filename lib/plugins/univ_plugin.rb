32c84de5f86869a43c9a91024b68c0337e17ad1a
Kasumi Hanazuki <kasumi@rollingapple.net>
2014-06-17 03:00:15 +0900

    Fix regression

--- lib/plugins/univ_plugin.rb
+++ lib/plugins/univ_plugin.rb
@@ -4,7 +4,7 @@ require_relative 'univ_helpers'
 module GuildBook
   class App
     get '/!univ' do
-      users = user_repo.find(nil, false).sort_by {|u| u['uidNumber'].first }
+      users = user_repo.search(nil, false).sort_by {|u| u['uidNumber'].first }
       haml :univ, locals: {users: users}, layout: false
     end
   end
