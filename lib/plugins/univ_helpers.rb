30225f8b92152659f8b5dad72bc1e2120e094a69
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-03-13 23:38:21 +0900

    UnivPluging: 下宿生は「下宿」と表示する

--- lib/plugins/univ_helpers.rb
+++ lib/plugins/univ_helpers.rb
@@ -45,7 +45,7 @@ module GuildBook
       end
 
       def lodging(u)
-        u['x-kmc-Lodging'].first == 'FALSE' ? '' : '○'
+        u['x-kmc-Lodging'].first == 'FALSE' ? '' : '下宿'
       end
 
       def address(u)
