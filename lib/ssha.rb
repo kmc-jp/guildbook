135c65dc756bd812bf0311f05230fa6b7a8f44d8
Tomoki UDA <tuda@kmc.gr.jp>
2015-04-18 12:17:38 +0900

    makes salt generation safer

--- lib/ssha.rb
+++ lib/ssha.rb
@@ -1,11 +1,12 @@
 
+require 'securerandom'
 require 'digest/sha1'
 
 module Sha1
   module_function
   def ssha_hash(password)
     salt_charset = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    salt = salt_charset[rand 64] + salt_charset[rand 64]
+    salt = Array.new(4) { salt_charset[SecureRandom.random_number(salt_charset.length)] }.join
     '{SSHA}' + Base64.encode64(Digest::SHA1.digest(password + salt) + salt).chomp
   end
 end
