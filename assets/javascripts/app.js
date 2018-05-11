ce8b790b658f88c0a60c6274858363ad11daa0ca
Kasumi Hanazuki <kasumi@rollingapple.net>
2018-05-12 01:28:41 +0900

    Client-side validation of SSH public keys

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,2 +1,18 @@
 //= require jquery
 //= require bootstrap
+
+jQuery(document).ready(function($) {
+  const pubkeyPattern = /^(?:ssh-rsa|ecdsa-sha2-nistp256|ecdsa-sha2-nistp384|ecdsa-sha2-nistp521|ssh-ed25519) /;
+
+  function validateSshPublicKey() {
+    for (const [i, pubkey] of this.value.trim().split(/\r?\n/).entries()) {
+      if(!pubkeyPattern.test(pubkey)) {
+        this.setCustomValidity(`${i+1}行目の公開鍵がおかしいです`);
+        return;
+      }
+    }
+    this.setCustomValidity('');
+  }
+
+  $('textarea.ssh-public-key').on('input', validateSshPublicKey);
+});
