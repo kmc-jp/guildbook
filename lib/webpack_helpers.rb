ae89e6595053c43a2f94ce42010a3765f3728b5f
Kasumi Hanazuki <kasumi@rollingapple.net>
2020-04-16 05:00:51 +0000

    <script defer>

--- lib/webpack_helpers.rb
+++ lib/webpack_helpers.rb
@@ -2,14 +2,14 @@ require 'cgi'
 require 'json'
 
 module WebpackHelpers
-  def javascript_tag(name)
-    html = asset_uris(name, 'js').map {|uri|  %{<script src="#{CGI.escapeHTML(uri)}"></script>} }.join
+  def javascript_tag(name, attrs = {})
+    html = asset_uris(name, 'js').map {|uri| %{<script src="#{CGI.escapeHTML(uri)}"#{format_attrs(attrs)}></script>} }.join
     html = html.html_safe if html.respond_to?(:html_safe)
     html
   end
 
-  def stylesheet_tag(name)
-    html = asset_uris(name, 'css').map {|uri|  %{<link rel="stylesheet" href="#{CGI.escapeHTML(uri)}">} }.join
+  def stylesheet_tag(name, attrs = {})
+    html = asset_uris(name, 'css').map {|uri| %{<link rel="stylesheet" href="#{CGI.escapeHTML(uri)}"#{format_attrs(attrs)}>} }.join
     html = html.html_safe if html.respond_to?(:html_safe)
     html
   end
@@ -42,5 +42,18 @@ module WebpackHelpers
 
   def assets_base_uri
     @assets_base_uri ||= ENV.fetch('WEBPACK_DEV_SERVER_URL', settings.assets_uri)
+  end
+
+  def format_attrs(attrs)
+    attrs.map do |k, v|
+      case v
+      when true
+        %{ #{CGI.escapeHTML(k.to_s)}}
+      when false
+        nil
+      else
+        %{ #{CGI.escapeHTML(k.to_s)}=#{CGI.escapeHTML(v.to_s)}}
+      end
+    end.compact.join
   end
 end
