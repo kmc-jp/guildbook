aa1b8d68b4b684dfe242a999ca0cc98c8f475ed1
walnuts1018 <r.juglans.1018@gmail.com>
2023-04-30 01:50:38 +0900

    学生番号と所属について追加

--- lib/view_helpers.rb
+++ lib/view_helpers.rb
@@ -4,11 +4,22 @@ module Haml
       opt[:class] ||= 'form-control'
       haml_tag :input, {name: attr, value: user[attr].first}.merge(opt)
     end
-    def select_userattr(user, attr, opt = {})
+    def select_userattr(user, attr, values, opt = {})
       opt[:class] ||= 'form-select'
       haml_tag :select, {name: attr}.merge(opt) do
-        haml_tag :option, selected: (user[attr].first=="TRUE"), value: "TRUE" do haml_concat "京都大学" end
-        haml_tag :option, selected: (user[attr].first=="FALSE"), value: "FALSE" do haml_concat "その他" end
+        if (user[attr].size==0) then
+          values.each{|k, v|
+            haml_tag :option, value: k do haml_concat v end
+          }
+        else
+          values.each{|k, v|
+            if (user[attr].first.to_sym==k) then
+              haml_tag :option, selected: true, value: k do haml_concat v end
+            else
+              haml_tag :option, value: k do haml_concat v end
+            end
+          }
+        end
       end
     end
   end
