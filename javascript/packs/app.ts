ce510629c68798085619ec6ea4c9a14108316d79
Walnuts <r.juglans.1018@gmail.com>
2023-05-02 23:14:49 +0900

    Update javascript/packs/app.ts
    Co-authored-by: Kasumi Hanazuki <kasumi@rollingapple.net>

--- javascript/packs/app.ts
+++ javascript/packs/app.ts
@@ -218,7 +218,7 @@ document.addEventListener('DOMContentLoaded', () => {
         form.querySelectorAll<HTMLDivElement>('form div.not-ku').forEach(div => {
           div.hidden = isKUMember;
         })
-        form.querySelectorAll<HTMLInputElement>('form div.is-ku .form-control, .form-select').forEach(input => {
+        form.querySelectorAll<HTMLInputElement>('form div.is-ku .form-control, form div.is-ku .form-select').forEach(input => {
           input.required=isKUMember;
         })
         form.querySelectorAll<HTMLInputElement>('form div.not-ku .form-control, .form-select').forEach(input => {
