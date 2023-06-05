cc055ad4dafa05cf90cfa32ea5fc51a9ca5b381a
hiromi-mi <31833325+hiromi-mi@users.noreply.github.com>
2023-06-06 00:20:41 +0900

    use input.required instead of setAttribute/removeAttribute

--- javascript/packs/app.ts
+++ javascript/packs/app.ts
@@ -219,25 +219,13 @@ document.addEventListener('DOMContentLoaded', () => {
           div.hidden = isKUMember;
         })
         form.querySelectorAll<HTMLInputElement>('form div.is-ku:not(.not-required) .form-control, form div.is-ku:not(.not-required) .form-select').forEach(input => {
-          if (isKUMember) {
-            input.setAttribute("required", "true");
-          } else {
-            input.removeAttribute("required");
-          }
+          input.required = isKUMember;
         })
         form.querySelectorAll<HTMLInputElement>('form div.grade .form-control, form div.grade .form-select').forEach(input => {
-          if (isKUMember) {
-            input.setAttribute("required", "true");
-          } else {
-            input.removeAttribute("required");
-          }
+          input.required = isKUMember;
         })
         form.querySelectorAll<HTMLInputElement>('form div.not-ku:not(.not-required) .form-control, form div.not-ku:not(.not-required) .form-select').forEach(input => {
-          if (!isKUMember) {
-            input.setAttribute("required", "true");
-          } else {
-            input.removeAttribute("required");
-          }
+          input.required = !isKUMember;
         })
       }
       rewrite();
